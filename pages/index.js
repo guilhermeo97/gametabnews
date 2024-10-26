9dcfff0f0bde01426d51ed1fe1f45b75b2ea8d01
Guilherme Oliveira <55219644+guilhermeo97@users.noreply.github.com>
2024-10-26 22:44:55 +0000

    feat: frase pra alguém

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home(){
+    return(
+        <>
+            <h1>Uma frase pra você</h1>
+            <p>
+                Seja clara no que você deseja, perseverante com os seus sonhos e grata pelas suas conquistas.
+            </p>
+        </>
+        
+        
+    )
+}
