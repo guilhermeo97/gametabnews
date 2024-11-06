6220fc2ed86eeb0b5d57fbb9e5c6f2c655c2dbdf
Guilherme Oliveira <55219644+guilhermeo97@users.noreply.github.com>
2024-11-06 00:17:51 +0000

    ajuste index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
-// export default function Home() {
-//   return (
-//     <>
-//       <h1>Uma frase pra você</h1>
-//       <p>Uma nova frase</p>
-//     </>
-//   );
-// }
-
-function teste() {
-  console.log("teste");
+export default function Home() {
+  return (
+    <>
+      <h1>Uma frase pra você</h1>
+      <p>Uma nova frase</p>
+    </>
+  );
 }
