607bf4a98ac6fa5f6fbcea4c780ca8183b0eae84
Guilherme Oliveira <55219644+guilhermeo97@users.noreply.github.com>
2024-11-30 22:32:26 +0000

    adicionado arquivo database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import DataBase from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await DataBase.query("SELECT 1 + 1;");
   response.status(200).json("status");
 }
 
