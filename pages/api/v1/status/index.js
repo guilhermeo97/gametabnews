82df12d4b4f62ec8306ab769362c1937ccc295a8
Guilherme Oliveira <55219644+guilhermeo97@users.noreply.github.com>
2025-04-02 01:45:29 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,36 @@
+import { version } from "react";
 import DataBase from "../../../../infra/database";
 
 async function status(request, response) {
-  const result = await DataBase.query("SELECT 1 + 1;");
-  response.status(200).json("status");
+  const dataBaseName = process.env.POSTGRES_DB;
+  const updateAt = new Date().toISOString();
+  const databaseVersionResult = await DataBase.query("SHOW server_version;");
+  const databaseMaxConnectionsResult = await DataBase.query(
+    "SHOW max_connections;",
+  );
+  const databaseOpenedConnectionsResult = await DataBase.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dataBaseName],
+  });
+  const databaseVersionResultValue =
+    databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResultValue,
+        max_connections: +databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
